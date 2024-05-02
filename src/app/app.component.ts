66994d122e3d0579d99b00484b9518b58f0fac40
Ergy Zenzo <ergyzenzo.dev@gmail.com>
2024-05-02 16:43:50 +0200

    First commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,35 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { button } from './button';
+import { CommonModule, NgIf } from '@angular/common';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, NgIf, CommonModule],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
 export class AppComponent {
   title = 'directives';
+
+  isAdmin: boolean = true;
+
+  mainTitle: string = 'Bonjour !';
+  
+  button: button = {
+    label: 'View',
+  }
+  
+  button2: button = {
+    label: 'Edit',
+  }
+
+  button3: button = {
+    label: 'Connect Admin',
+  }
+
+  toggleAdmin(){
+    this.isAdmin = !this.isAdmin;
+  }
 }
